18d4a595c4dd909c1a352602af2167a55039833a
Nobuki Yoda <nobuki.y@gmail.com>
2014-12-24 23:30:01 +0900

    First Commit.

---
+++ Assets/VertexEffects/Scripts/ModifiedShadow.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+/// <summary>
+/// Capacityの計算がおかしいので、修正されるまではこれを使う。
+/// </summary>
+public class ModifiedShadow : Shadow
+{
+    protected new void ApplyShadow(List<UIVertex> verts, Color32 color, int start, int end, float x, float y)
+    {
+        UIVertex vt;
+
+        var neededCpacity = verts.Count + (end - start);
+        if (verts.Capacity < neededCpacity)
+            verts.Capacity = neededCpacity;
+
+        for (int i = start; i < end; ++i)
+        {
+            vt = verts[i];
+            verts.Add(vt);
+
+            Vector3 v = vt.position;
+            v.x += x;
+            v.y += y;
+            vt.position = v;
+            var newColor = color;
+            if (useGraphicAlpha)
+                newColor.a = (byte)((newColor.a * verts[i].color.a) / 255);
+            vt.color = newColor;
+            verts[i] = vt;
+        }
+    }
+}
